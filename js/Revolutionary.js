33446f2f5564c82e1057890ab6b58660ffe24b3a
Douglas Fleischut <df@douglass-imac.lan>
2020-05-24 15:21:06 -0700

    first commit

---
+++ js/Revolutionary.js
@@ -0,0 +1,10 @@
+class Revolutionary {
+    constructor(...args){
+        this.name = args[0];
+        this.imgPath = args[1];
+        this.born = args[2];
+        this.died = args[3];
+        this.desc=args[4];
+    }
+
+}
---
+++ js/index.js
@@ -0,0 +1,19 @@
+
+
+
+    function preload(){
+        loadImage(img/)
+    }
+
+
+
+
+    function setup(){
+
+        const myCanvas = createCanvas(window.innerWidth,1200);
+        background(130, 130, 130);
+        // myCanvas.parent("mainRow");
+    }
+
+
+
